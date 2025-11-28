e3d31b3ff074ab3094308cd1a044b5ce008b19e0
RezaNurRochmat13 <rezanurrochmat@gmail.com>
2025-11-28 21:53:40 +0700

    feat(users): fix error user service

--- src/main/java/com/spring/unictive/module/user/service/UserServiceImpl.java
+++ src/main/java/com/spring/unictive/module/user/service/UserServiceImpl.java
@@ -6,9 +6,11 @@ import com.spring.unictive.utils.exception.ResourceNotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
